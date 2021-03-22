34d65e931c371ace90e1d94b3dbc94e1570add87
NebelGG <nebelggwp@gmail.com>
2021-03-22 19:16:55 +0300

    Домашнее задание к 8 уроку

---
+++ items.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class GbParseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class Insta(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
+
+
+class InstaTag(Insta):
+    pass
+
+
+class InstaPost(Insta):
+    pass
